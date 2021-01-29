15afd42f52bc4938d6719c0281be42376a2c825e
joan.jimenez <joan.jimenez@solenium.co>
2021-01-28 22:07:26 -0500

    actualizacion

--- app.py
+++ app.py
@@ -213,7 +213,7 @@ def receive_message():
                     # Facebook Messenger ID for user so we know where to send response back to
                     recipient_id = message['sender']['id']
                     if message['message'].get('text'):
-                        response_sent_text = get_message()
+                        response_sent_text = get_message(message['message'].get('text'))
                         send_message(recipient_id, response_sent_text)
                     # if user send us a GIF, photo, video or any other non-text item
                     if message['message'].get('attachments'):
@@ -230,11 +230,11 @@ def verify_fb_token(token_sent):
     return 'Invalid verification token'
 
 
-def get_message():
+def get_message(message):
     sample_responses = ["You are stunning!", "We're proud of you",
                         "Keep on being you!", "We're greatful to know you :)"]
     # return selected item to the user
-    return random.choice(sample_responses)
+    return generate_response(message)
 
 
 # Uses PyMessenger to send response to the user
@@ -242,6 +242,7 @@ def send_message(recipient_id, response):
     # sends user the text message provided via input response parameter
     bot.send_text_message(recipient_id, response)
     return "success"
+
 
 #load model #1
 chatbotmodel = load_model('training_model.h5')
